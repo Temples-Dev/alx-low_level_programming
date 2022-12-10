47f1e507dd36d42e70db6b5508e250f0249a4629
Temples-Dev <templelomotey@gmail.com>
2022-12-10 06:12:13 -0800

    fixed betty doc style

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -9,8 +9,8 @@
 int main(void)
 {
 int base10;
-for (base10 = 0;base10 < 10; base10++)
-printf("%d",base10);
+for (base10 = 0; base10 < 10; base10++)
+printf("%d", base10);
 
 putchar('\n');
 return (0);
