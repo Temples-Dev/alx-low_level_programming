6d638aa6fb52448981cd2061a935c33c4fd28417
Temples-Dev <templelomotey@gmail.com>
2022-12-13 22:07:50 -0800

    betty style fixed, variable corrected

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,22 +1,23 @@
 #include "main.h"
 
 /**
- * void print_alphabet_x10 - a function that prints 10 times the alphabet, in lowercase
+ * print_alphabet_x10 - a function that prints 10 times the alphabet
  *
  * Return: void type
  */
 
 void print_alphabet_x10(void)
 {
-int aphaCount = 0;
+int alphaCount = 0;
 
-while (alphaCount <= 10){
+while (alphaCount <= 10)
+{
 char alpha = 'a';
 
 for (; alpha <= 'z'; alpha++)
 _putchar(alpha);
 
-  }
+}
 _putchar('\n');
 return;
 }
