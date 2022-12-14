bf5c1447d2f7ce88accd9aed1fd74ec3802284ed
Temples-Dev <templelomotey@gmail.com>
2022-12-13 21:04:42 -0800

    updated main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,18 +1,1 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <unistd.h>
-/**
- * _putchar - writes the character c to standard output
- *
- * @c: is the character to be printed
- *
- * Return: Always 1 (standard output)
- */
-
-int _putchar(char c)
-{
-return (write(1, &c, 1));
-}
-
-#endif /*MAIN_H*/
+int _putchar(char c);
