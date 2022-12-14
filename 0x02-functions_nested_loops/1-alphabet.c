dede9ca92f9ded20112ba2e830c1989c0e45b685
Temples-Dev <templelomotey@gmail.com>
2022-12-13 21:41:55 -0800

    added print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet in lowercase
+ *
+ * Return: void type
+ */
+
+void print_alphabet(void)
+{
+char alphabet = 'a';
+
+for (; alhabet <= 'z'; alphabet++)
+_putchar(alphabet);
+
+_putchar('\n');
+return;
+}
