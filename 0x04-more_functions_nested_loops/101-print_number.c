92d074575e398f457b14c41dec1828ff99458efa
Temples-Dev <templelomotey@gmail.com>
2022-12-17 03:23:17 -0800

    a function that prints an integer using _putchar

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -3,34 +3,35 @@
 /**
  * print_number - print an integer, without using long, arrays, or pointers
  *
- * @n: number to be printed
+ * @n: integer n
  */
 
 void print_number(int n)
 {
-unsigned int tens, digit, positive = n;
-double t_beg = 1;
-
 if (n == 0)
+{
 _putchar('0');
+}
 else
 {
 if (n < 0)
 {
-positive = n * -1;
 _putchar('-');
+n = -n;
 }
 
-while (t_beg <= positive)
-t_beg *= 10;
-tens = t_beg / 10;
-
-while (tens >= 1)
+int divisor = 1;
+while (n / divisor >= 10)
 {
-digit = positive / tens;
+divisor *= 10;
+}
+
+while (divisor >= 1)
+{
+int digit = n / divisor;
 _putchar(digit + '0');
-positive = (positive - (tens * digit));
-tens /= 10;
+n = n % divisor;
+divisor /= 10;
 }
 }
 }
