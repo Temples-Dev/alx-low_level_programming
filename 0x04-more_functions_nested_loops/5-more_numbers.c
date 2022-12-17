eace194a4f7f68def2abaf2e2979f2b0b3c8d6db
Temples-Dev <templelomotey@gmail.com>
2022-12-17 04:52:07 -0800

    updated

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,10 +1,14 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * more_numbers - print 0 to 14 ten times
  *
+ * @num: input from var i
+ *
  * Return: void data type
  */
+void print_two_digits(char num);
 
 void more_numbers(void)
 {
@@ -15,9 +19,27 @@ for (counter = 1; counter <= 10; counter++)
 {
 for (i = 0; i <= 14; i++)
 {
-_putchar(i + '0');
+if (i < 10)
+{
+putchar(i + '0');
+}
+else
+{
+print_two_digits(i);
+}
 }
 _putchar('\n');
 }
 }
 
+/**
+ * print_two_digits - splits 10-14 and print
+ *
+ * @num: input from var i
+ */
+
+void print_two_digits(char num)
+{
+putchar((num / 10) + '0');
+putchar((num % 10) + '0');
+}
