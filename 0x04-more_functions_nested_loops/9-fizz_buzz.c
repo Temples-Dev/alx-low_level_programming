9142ec3f8bb99e33d419215e8a3ebb4d9587ec3a
Temples-Dev <templelomotey@gmail.com>
2022-12-17 05:49:36 -0800

    betty fixed

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,21 +5,30 @@
  * multiples of 3 with Fizz,
  * multiples of 5 with Buzz
  * and multiple of both 3 and 5 with FizzBuzz
- * Return: Always 0 
+ * Return: Always 0
  */
 
-int main(void) {
-for (int i = 1; i <= 100; i++) {
-if (i % 3 == 0 && i % 5 == 0) {
+int main(void)
+{
+for (int i = 1; i <= 100; i++)
+{
+if (i % 3 == 0 && i % 5 == 0)
+{
 printf("FizzBuzz ");
-} else if (i % 3 == 0) {
+}
+else if (i % 3 == 0)
+{
 printf("Fizz ");
-} else if (i % 5 == 0) {
+}
+else if (i % 5 == 0)
+{
 printf("Buzz ");
-} else {
+}
+else
+{
 printf("%d ", i);
 }
 }
 printf("\n");
-return 0;
+return (0);
 }
