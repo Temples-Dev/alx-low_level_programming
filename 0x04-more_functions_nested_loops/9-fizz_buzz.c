0bce52a27bd4736c36bb5bdcb2f918d3ac060b2b
Temples-Dev <templelomotey@gmail.com>
2022-12-17 05:30:18 -0800

    updated

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,16 +12,20 @@ int main(void)
 {
 int i;
 
-for (i = 1; i <= 100; i++)
-{
 if (i % 3 == 0 || i % 5 == 0)
-printf("FizzBuzz");
-else if (i % 3 == 0)
+{
+if (i % 3 == 0)
+{
 printf("Fizz");
-else if (i % 5 == 0)
+}
+if (i % 5 == 0)
+{
 printf("Buzz");
-else
+}
+} else
+{
 printf("%d", i);
+}
 
 if (i < 100)
 printf(" ");
