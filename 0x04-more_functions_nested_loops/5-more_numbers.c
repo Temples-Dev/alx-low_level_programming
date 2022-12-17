f669cf720c750821120641934a7968cbcee1a212
Temples-Dev <templelomotey@gmail.com>
2022-12-17 03:59:42 -0800

    fixed _putchar

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,8 +11,8 @@ void more_numbers(void)
 int count = 1;
 do {
 for (int j = 0; j <= 14; j++)
-putchar(j + '0');
+_putchar(j + '0');
 
-putchar('\n');
+_putchar('\n');
 count++;
 } while (count <= 14);
