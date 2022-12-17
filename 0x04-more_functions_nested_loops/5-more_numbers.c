35edeae2ac329e45803a67178843cb0f95765ab7
Temples-Dev <templelomotey@gmail.com>
2022-12-16 23:59:52 -0800

    updated

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,15 +9,18 @@
 void more_numbers(void)
 {
 int count = 1;
-int num;
+char num;
 do {
 for (num = 0; num <= 14; num++)
 {
-_putchar('0' + num);
+if (num / 10 > 0)
+_putchar((num / 10) + '0');
+
+_putchar((num % 10) + '0');
+
 }
 _putchar('\n');
 count++;
 } while (count <= 14);
 
-_putchar('\n');
 }
