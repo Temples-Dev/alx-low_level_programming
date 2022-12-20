6a1806252c057dd6fd59a889016dc700ea670137
Temples-Dev <templelomotey@gmail.com>
2022-12-19 18:39:41 -0800

    increment changed

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -29,7 +29,7 @@ void puts2(char *str)
 {
 int i;
 
-for (i = 0; str[i] != '\0' && i < _strlen(str); i++)
+for (i = 0; str[i] != '\0' && i < _strlen(str); i += 2)
 _putchar(str[i]);
 
 _putchar('\n');
