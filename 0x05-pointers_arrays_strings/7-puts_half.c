c0964e00f33fa8bdc2ba69bd88360dd56f527018
Temples-Dev <templelomotey@gmail.com>
2022-12-19 19:47:13 -0800

    prints half of a string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,43 +1,34 @@
 #include "main.h"
 
 /**
- * _strlen - returns the length of a string
- * @s: string
- * Return: length
- */
-
-int _strlen(char *s)
-{
-int len = 0;
-
-while (*s != '\0')
-{
-len++;
-s++;
-}
-
-return (len);
-}
-
-/**
- * puts_half - prints half of a string, followed by a new line
+ * puts_half - prints the half of a string
  * @str: string to print
  */
-
 void puts_half(char *str)
 {
-int index;
-int len = _strlen(str);
+int i;
+int length = 0;
 
-if (len % 2 != 0)
-index = ((len - 1) / 2);
-else
-index = (len / 2);
-
-while (index < len)
+/* find the length of the string */
+while (str[length] != '\0')
 {
-_putchar(*(str + index));
-index++;
+length++;
+}
+
+/* find the index to start printing from,*/
+/* depending on the even/odd length of the string */
+if (length % 2 != 0)
+i = (length - 1) / 2 + 1;
+else
+i = length / 2 + 1;
+
+/* print the second half of the string, minus the first character */
+while (i < length)
+{
+_putchar(str[i]);
+i++;
 }
 _putchar('\n');
 }
+
+
