318d55fb8d32095d27721bb3057377f7d60be99e
Temples-Dev <templelomotey@gmail.com>
2023-01-06 20:27:03 -0800

    update

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -13,6 +13,9 @@ int substring_match(char *s1, char *s2, char *wldcd)
 if (*s1 == '\0' && *s2 == '\0')
 return (1);
 
+if (*s1 == '\0' && *s2 == '*')
+return (substring_match(s1, s2 + 1, s2 + 1));
+
 if (*s1 == '\0' && *s2 != '\0')
 return (0);
 
