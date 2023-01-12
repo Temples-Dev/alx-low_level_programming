381179a41d3d7ed1aa7db67f98b007ab3d9b0650
Temples-Dev <templelomotey@gmail.com>
2023-01-12 00:17:44 -0800

    updated

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -5,11 +5,10 @@
  * @s: string
  * Return: string length
  */
-
 int _strlen_recursion(char *s)
 {
-if (*s != '\0')
-s++;
-
-return (1 + _strlen_recursion(s));
+if (*s == '\0')
+return (0);
+else
+return (1 + _strlen_recursion(s + 1));
 }
