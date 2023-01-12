86bd8861e783601ef697d7f1b60a7f071852fb1d
Temples-Dev <templelomotey@gmail.com>
2023-01-12 02:31:54 -0800

    updated

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,6 +2,48 @@
 #include <stdlib.h>
 
 /**
+ * make_change - finds least number of coins
+ * @n: money amount
+ * Return: number of coins
+ */
+
+int make_change(int n)
+{
+int coins = 0;
+
+while (n > 0)
+{
+if (n >= 25)
+{
+n -= 25;
+coins++;
+}
+else if (n >= 10)
+{
+n -= 10;
+coins++;
+}
+else if (n >= 5)
+{
+n -= 5;
+coins++;
+}
+else if (n >= 2)
+{
+n -= 2;
+coins++;
+}
+else
+{
+n -= 1;
+coins++;
+}
+}
+
+return (coins);
+}
+
+/**
  * main - prints the minimum number of coins to make change for an amount.
  * of money.
  * @argc: number of command line arguments.
@@ -11,40 +53,19 @@
 
 int main(int argc, char *argv[])
 {
-int cents, totalCoins = 0;
-
-if (argc == 1 || argc > 2)
+if (argc != 2)
 {
 printf("Error\n");
 return (1);
 }
-
-cents = atoi(argv[1]);
-
-while (cents > 0)
+else if (atoi(argv[1]) < 0)
 {
-if (cents >= 25)
-{
-cents -= 25;
-}
-else if (cents >= 10)
-{
-cents -= 10;
-else if (cents >= 5)
-{
-cents -= 5;
-}
-else if (cents >= 2)
-{
-cents -= 2;
+printf("0\n");
 }
 else
 {
-cents -= 1;
-}
-totalCoins += 1;
-}
-
+int totalCoins = make_change(atoi(argv[1]));
 printf("%d\n", totalCoins);
+}
 return (0);
 }
