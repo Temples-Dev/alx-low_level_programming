6cbda16ba9e487477ba1c1bd5e12343aca28cd20
Temples-Dev <templelomotey@gmail.com>
2023-01-14 00:03:10 -0800

    updated

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,15 +1,13 @@
 #include <stdlib.h>
+#include "main.h"
 
 /**
  * string_nconcat - function that concatenates two strings
- *
  * @s1: string 1
  * @s2: string 2
- * @n: bytes of @s2 to add to @s1 to be a new string
- *
- * Return: new string followed by the first @n bytes
- *         of string 2 @s2 or NULL
- */
+ * @n: n amount of bytes
+ * Return: new string
+ **/
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
