e851481d67cadd2068110b98f33076d2488e62df
Temples-Dev <templelomotey@gmail.com>
2023-01-27 09:00:27 -0800

    updated

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,35 +11,36 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-list_t *new_node;
-size_t str_len = 0;
+	list_t *new_node;
+	size_t str_len = 0;
 
-if (str == NULL)
-str_len = 0;
+	/*set strig length to 0 if its NULL*/
+	if (str == NULL)
+		str_len = 0;
 
+	/*count length of string*/
+	while (str[str_len] != '\0')
+		str_len++;
 
-while (str[str_len] != '\0')
-str_len++;
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
 
-new_node = malloc(sizeof(list_t));
-if (new_node == NULL)
-return (NULL);
+	/**
+	 * initialize the next addr of new_node
+	 * to NULL if head is NULL, if it's not
+	 * move the first node to the addr of new_node
+	 * ->next hence adding new_node at the beginning
+	 *  of the list
+	*/
+	if (*head == NULL)
+		new_node->next = NULL;
+	else
+		new_node->next = *head;
 
-/**
- * initialize the next addr of new_node
- * to NULL if head is NULL, if it's not
- * move the first node to the addr of new_node
- * ->next hence adding new_node at the beginning
- *  of the list
-*/
-if (*head == NULL)
-new_node->next = NULL;
-else
-new_node->next = *head;
+	new_node->str = strdup(str);
+	new_node->len = str_len;
+	*head = new_node;
 
-new_node->str = strdup(str);
-new_node->len = str_len;
-*head = new_node;
-
-return (*head);
+	return (*head);
 }
